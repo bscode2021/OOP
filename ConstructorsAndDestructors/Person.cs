464afb1b090efe2b78b3299daf2c03572453ed6d
bscode2021 <bscode@hotmail.com>
2022-09-22 19:21:33 +0200

    Add project files.

---
+++ ConstructorsAndDestructors/Person.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstructorsAndDestructors
+{
+    public class Person
+    {
+        public Person(string name)
+        {
+            Name = name;
+        }
+        private string Name;
+        public override string ToString()
+        {
+            return Name;
+        }
+
+        ~Person()
+        {
+            Name = string.Empty;
+        }
+    }
+}
---
+++ ConstructorsAndDestructors/Program.cs
@@ -0,0 +1,17 @@
+﻿using ConstructorsAndDestructors;
+
+Person[] people = new Person[3];
+
+for (int i = 0; i < 3; i++)
+{
+    Console.WriteLine("Enter a friend first name: ");
+
+    Person person = new Person(Console.ReadLine());
+
+    people[i] = person;
+}
+Console.WriteLine();
+for (int i = 0; i < 3; i++)
+{
+    Console.WriteLine($"Hello! my name is {people[i].ToString()}");
+}
