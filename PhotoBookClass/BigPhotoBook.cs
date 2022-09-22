afc614ae3a1b7b441438abc451453dd7b13e740a
bscode2021 <bscode@hotmail.com>
2022-09-22 20:33:40 +0200

    added PhotoBookClass project

---
+++ PhotoBookClass/BigPhotoBook.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhotoBookClass
+{
+    public class BigPhotoBook : PhotoBook
+    {
+        public BigPhotoBook() : base(64)
+        {  }
+    }
+}
---
+++ PhotoBookClass/PhotoBookTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhotoBookClass
+{
+    public static class PhotoBookTest
+    {
+        public static void ShowBooks()
+        {
+            PhotoBook PhotoBook16 = new PhotoBook();
+            PhotoBook PhotoBook24 = new PhotoBook(24);
+            BigPhotoBook BigPhotoBook = new BigPhotoBook();
+
+            Console.WriteLine(PhotoBook16.GetNumberPages());
+            Console.WriteLine(PhotoBook24.GetNumberPages());
+            Console.WriteLine(BigPhotoBook.GetNumberPages());
+        }
+    }
+}
