da572c428bd302d329ff6b9509a36f2610d754f9
bscode2021 <bscode@hotmail.com>
2022-09-23 20:03:11 +0200

    added Interfaces project

---
+++ Interfaces/Car.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interfaces
+{
+    public class Car : IVehicle
+    {
+        public int carTank { get; set; }
+
+        public Car()
+        {
+            carTank = 0;
+        }
+        public void Drive()
+        {
+            if (carTank < 1)
+            {
+                Console.WriteLine("Not Driving ");
+            }
+            else
+            {
+                Console.WriteLine("Driving");
+            }
+        }
+
+        public bool Refuel(int liters)
+        {
+            carTank += liters;
+            return true;
+        }
+    }
+}
