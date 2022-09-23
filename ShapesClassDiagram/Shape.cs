99158458a9008f4f0d32fc09f33c302b5ae0845d
bscode2021 <bscode@hotmail.com>
2022-09-23 18:45:06 +0200

    Added ShapesClassDiagram Project

---
+++ ShapesClassDiagram/Shape.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShapesClassDiagram
+{
+    public class Shape
+    {
+        protected Location Location { get; set; }
+
+        public override string ToString()
+        {
+            return "";
+        }
+
+        public double Area()
+        {
+            return 1.1;
+        }
+
+        public double Parametar()
+        {
+            return 2.2;
+        }
+    }
+}
