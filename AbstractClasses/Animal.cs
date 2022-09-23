f350ff166444d731301128e2ff67996660315540
bscode2021 <bscode@hotmail.com>
2022-09-23 20:26:41 +0200

    Added AbstractClasses project

---
+++ AbstractClasses/Animal.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractClasses
+{
+    public abstract class Animal
+    {
+        private string _name;
+
+        public bool SetName(string name)
+        {
+            _name = name;
+            return true;
+        }
+        public string GetName()
+        {
+            return _name;
+        }
+
+        public abstract void Eat();
+    }
+}
